cbfb78ae8df54e733343e998767b91a36f5be84d
alariej <alaj@gmx.net>
2021-07-23 14:29:27 +0200

    electron-main: removed on activate listener, and added quit code

--- electron-main.js
+++ electron-main.js
@@ -13,9 +13,9 @@ const createWindow = () => {
         frame: true,
         movable: true,
         backgroundColor: 'black',
-        icon: path.join(__dirname, 'dist-web', 'resources', 'images', 'logo.png'), // linux
+        // icon: path.join(__dirname, 'dist-web', 'resources', 'images', 'logo.png'), // linux
         // icon: path.join(__dirname, 'dist-web', 'resources', 'images', 'logo.ico'), // windows
-        // icon: path.join(__dirname, 'dist-web', 'resources', 'images', 'logo.icns'), // mac
+        icon: path.join(__dirname, 'dist-web', 'resources', 'images', 'logo.icns'), // mac
         webPreferences: {
             nodeIntegration: true, // not used in electron v12
             contextIsolation: false, // required in electron v12
@@ -51,7 +51,12 @@ const createWindow = () => {
 
     mainWindow.on('close', event => {
         event.preventDefault();
-        if (mainWindow) { mainWindow.webContents.send('storeDataAndCloseApp') } // eslint-disable-line
+
+        if (mainWindow) {
+            mainWindow.webContents.send('storeDataAndCloseApp') // eslint-disable-line
+        } else {
+            app.quit();
+        }
     });
 }
 
@@ -59,9 +64,11 @@ app.whenReady()
     .then(_response => { createWindow() })
     .catch(_error => null);
 
+/* 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) { createWindow() }
 });
+*/
 
 ipcMain.on('closeApp', () => {
     if (mainWindow) { mainWindow.removeAllListeners('close') } // eslint-disable-line
