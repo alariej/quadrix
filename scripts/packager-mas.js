60a1e66d8d4cf39c046b94517c0271841bd92d17
alariej <alaj@gmx.net>
2021-07-29 18:11:32 +0200

    electron mac: added packager script

---
+++ scripts/packager-mas.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+const packager = require('electron-packager');
+const setLanguages = require('electron-packager-languages');
+const package = require('../package.json');
+
+const options = {
+    dir: './',
+    appBundleId: 'chat.quadrix.mac',
+    out: './dist-mac',
+    platform: 'mas',
+    arch: 'x64',
+    overwrite: true,
+    icon: './macos/resources/icon.icns',
+    appVersion: package.version,
+    prune: true,
+    derefSymlinks: false,
+    ignore: ['^\/(?!(dist-web|macos|electron-main\.js|package\.json))'],
+    extendInfo: './macos/custom.plist',
+    extraResource: [
+        './macos/de.lproj',
+        './macos/en.lproj',
+        './macos/fr.lproj'
+    ],
+    afterCopy: [setLanguages([
+        'en', 'en-US', 'de', 'fr'
+    ])]
+};
+
+packager(options)
+    .then(path => console.log('MAS package available at ' + path))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
