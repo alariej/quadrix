445b14572f882b0e9bc0c1281ac24ac3cfead6f7
alariej <alaj@gmx.net>
2021-10-15 10:20:57 +0200

    metro.config: added jitsi sdk to blocklist

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,18 @@
 /* eslint-disable */
+
+const blocklist = require('metro-config/src/defaults/exclusionList');
+
 module.exports = {
     transformer: {
         getTransformOptions: async () => ({
-            transform: { experimentalImportSupport: false, inlineRequires: true },
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true
+            },
         }),
+        maxWorkers: 2,
+        resolver: {
+            blocklistRE: blocklist([/ios\/Pods\/JitsiMeetSDK\/Frameworks\/JitsiMeet.framework\/assets\/node_modules\/react-native\/.*/])
+        }
     },
 };
