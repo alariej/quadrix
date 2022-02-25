2d63446dcdf0cfbdd7446aa400add337d6290d69
alariej <alaj@gmx.net>
2022-02-25 17:05:57 +0100

    New interface model: UploadFileInfo

---
+++ src/models/UploadFileInfo.ts
@@ -0,0 +1,15 @@
+export interface ThumbnailInfo {
+    mimeType: string,
+    fileSize: number,
+    height: number,
+    width: number
+}
+
+export interface UploadFileInfo {
+    uri: string;
+    fileName?: string;
+    fileSize?: number;
+    mimeType?: string;
+    thumbnailUrl?: string;
+    thumbnailInfo?: ThumbnailInfo;
+}
