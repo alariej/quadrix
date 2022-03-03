134ef05254dc5e87ae4665bdf39d25e67f346ba0
alariej <alaj@gmx.net>
2022-03-03 15:55:44 +0100

    new view-based spinner

--- src/components/SvgSpinner.tsx
+++ src/components/SvgSpinner.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as RX from 'reactxp';
-import RXImageSvg, { SvgPath as RXSvgPath } from 'reactxp-imagesvg';
 
 interface SvgSpinnerProps {
     color: string;
@@ -45,22 +44,27 @@ export default class SvgSpinner extends RX.Component<SvgSpinnerProps, RX.Statele
 
         return (
             <RX.Animated.View style={ this.animatedStyle }>
-                <RXImageSvg
-                    viewBox={ '0 0 42.5 42.5' }
-                    height={ this.props.size }
-                    width={ this.props.size }
+                <RX.View
+                    style={{
+                        height: this.props.size,
+                        width: this.props.size,
+                        borderRadius: this.props.size / 2,
+                        borderWidth: this.props.size / 5,
+                        borderColor: this.props.color,
+                        alignItems: 'center',
+                    }}
                 >
-                    <RXSvgPath
-                        fillColor={ this.props.color }
-                        fillOpacity={ 0.8 }
-                        d={ 'M 21.25,1.25 A 20,20 0 0 0 1.25,21.25 20,20 0 0 0 21.25,41.25 20,20 0 0 0 41.25,21.25 20,20 0 0 0 21.25,1.25 Z m 0,2.5 A 17.5,17.5 0 0 1 38.75,21.25 17.5,17.5 0 0 1 21.25,38.75 17.5,17.5 0 0 1 3.75,21.25 17.5,17.5 0 0 1 21.25,3.75 Z' } // eslint-disable-line
+                    <RX.View
+                        style={{
+                            height: (this.props.size - (2 * this.props.size / 5)) / 2 + (this.props.size / 10) / 2,
+                            width: this.props.size / 10,
+                            borderBottomLeftRadius: this.props.size / 10,
+                            borderBottomRightRadius: this.props.size / 10,
+                            backgroundColor: this.props.color
+                        }}
                     />
-                    <RXSvgPath
-                        fillColor={ this.props.color }
-                        d={ 'M 21.25,0 A 21.25,21.25 0 0 0 6.233,6.233 L 9.766,9.766 A 16.25,16.25 0 0 1 21.25,5 16.25,16.25 0 0 1 32.733,9.766 L 36.266,6.233 A 21.25,21.25 0 0 0 21.25,0 Z' } // eslint-disable-line
-                    />
-                </RXImageSvg>
+                </RX.View>
             </RX.Animated.View>
-        );
+        )
     }
 }
