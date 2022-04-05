4b48ec9c352200d7d00a68a5d3623432937e1300
alariej <alaj@gmx.net>
2022-04-05 18:00:32 +0200

    Animated spinner: New design

--- src/components/AnimatedSpinner.tsx
+++ src/components/AnimatedSpinner.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as RX from 'reactxp';
-import { BUTTON_FILL } from '../ui';
 
 interface AnimatedSpinnerProps {
     color: string;
@@ -44,41 +43,35 @@ export default class AnimatedSpinner extends RX.Component<AnimatedSpinnerProps, 
     public render(): JSX.Element | null {
 
         const ringWidth = this.props.size / 5;
-        const baseRingOffset = 0; // this.props.size / 8;
-        const blueRingOffest = baseRingOffset + this.props.size / 6;
 
         return (
             <RX.Animated.View style={[
                 this.animatedStyle,
                 {
-                    height: this.props.size + blueRingOffest,
-                    width: this.props.size + blueRingOffest,
+                    height: this.props.size,
+                    width: this.props.size,
                     overflow: 'visible'
                 }
             ]}>
                 <RX.View
                     style={{
                         position: 'absolute',
-                        top: blueRingOffest,
-                        left: blueRingOffest / 2,
-                        height: this.props.size,
-                        width: this.props.size,
-                        borderRadius: this.props.size / 2,
-                        borderWidth: ringWidth,
-                        borderColor: BUTTON_FILL,
-                        opacity: 0.6
-                    }}
-                />
-                <RX.View
-                    style={{
-                        position: 'absolute',
-                        top: baseRingOffset,
-                        left: blueRingOffest / 2,
                         height: this.props.size,
                         width: this.props.size,
                         borderRadius: this.props.size / 2,
                         borderWidth: ringWidth,
                         borderColor: this.props.color,
+                        opacity: 0.4
+                    }}
+                />
+                <RX.View
+                    style={{
+                        position: 'absolute',
+                        left: (this.props.size - ringWidth) / 2,
+                        height: ringWidth,
+                        width: ringWidth,
+                        borderRadius: ringWidth / 2,
+                        backgroundColor: this.props.color,
                         opacity: 1
                     }}
                 />
