9ed1fb7696c283b7158a014656a5877f65d5a9b0
alariej <alaj@gmx.net>
2022-04-06 20:08:46 +0200

    Settings: reverts a previous commit to hide email settings on Android

--- src/dialogs/DialogSettings.tsx
+++ src/dialogs/DialogSettings.tsx
@@ -691,7 +691,7 @@ export default class DialogSettings extends ComponentBase<unknown, DialogSetting
         }
 
         let emailSettings: ReactElement | undefined;
-        if (this.emailAddress && this.platform !== 'android') {
+        if (this.emailAddress) {
             emailSettings = (
                 <RX.View style={ [styles.settingsContainer, { marginTop: SPACING }] }>
 
