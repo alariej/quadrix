40ffd75168a67a186ba163185edeec353328726d
alariej <alaj@gmx.net>
2022-04-15 16:43:15 +0200

    Added blocklist resolver for the electron folder (iOS issue)

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,15 @@
 /* eslint-disable */
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
 module.exports = {
     transformer: {
         getTransformOptions: async () => ({
             transform: { experimentalImportSupport: false, inlineRequires: true },
         }),
     },
+    resolver:{
+        blockList: exclusionList([
+            /electron\/.*/,
+        ]),
+    }
 };
