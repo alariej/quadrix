49892105beb9fcf42738f920dae5a007f5571914
alariej <alaj@gmx.net>
2022-06-14 16:10:55 +0200

    Reformatted with Prettier

--- electron/electron-main.js
+++ electron/electron-main.js
@@ -7,42 +7,45 @@
 const { app, BrowserWindow, Menu, ipcMain, dialog, screen, MenuItem, protocol } = require('electron');
 const path = require('path');
 
-protocol.registerSchemesAsPrivileged([{
-    scheme: 'app', privileges: { standard: true, secure: true, supportFetchAPI: true },
-}]);
+protocol.registerSchemesAsPrivileged([
+	{
+		scheme: 'app',
+		privileges: { standard: true, secure: true, supportFetchAPI: true },
+	},
+]);
 
 const disableSpellcheck = {
-    en: 'Disable spell checking',
-    de: 'Rechtschreibprüfung deaktivieren',
-    fr: 'Désactiver la correction orthographique'
-}
+	en: 'Disable spell checking',
+	de: 'Rechtschreibprüfung deaktivieren',
+	fr: 'Désactiver la correction orthographique',
+};
 
 const enableSpellcheck = {
-    en: 'Enable spell checking',
-    de: 'Rechtschreibprüfung aktivieren',
-    fr: 'Activer la correction orthographique'
-}
+	en: 'Enable spell checking',
+	de: 'Rechtschreibprüfung aktivieren',
+	fr: 'Activer la correction orthographique',
+};
 
 const selectLanguage = {
-    en: 'Select language...',
-    de: 'Sprache auswählen...',
-    fr: 'Choisir la langue...'
-}
+	en: 'Select language...',
+	de: 'Sprache auswählen...',
+	fr: 'Choisir la langue...',
+};
 
 let iconFile;
 switch (process.platform) {
-    case 'darwin':
-        iconFile = 'icon.icns';
-        break;
-    case 'win32':
-        iconFile = 'icon.ico';
-        break;
-    case 'linux':
-        iconFile = 'icon.png';
-        break;
-    default:
-        iconFile = 'icon.png';
-        break;
+	case 'darwin':
+		iconFile = 'icon.icns';
+		break;
+	case 'win32':
+		iconFile = 'icon.ico';
+		break;
+	case 'linux':
+		iconFile = 'icon.png';
+		break;
+	default:
+		iconFile = 'icon.png';
+		break;
 }
 
 let appLocale;
@@ -52,152 +55,158 @@ let mainWindow;
 
 let hasLock = true;
 if (process.platform !== 'darwin') {
-    const additionalData = { myKey: 'chat.quadrix.electron' };
-    hasLock = app.requestSingleInstanceLock(additionalData);
+	const additionalData = { myKey: 'chat.quadrix.electron' };
+	hasLock = app.requestSingleInstanceLock(additionalData);
 }
 
 if (!hasLock) {
-
-    app.quit();
-
+	app.quit();
 } else {
+	const createWindow = () => {
+		const primaryDisplay = screen.getPrimaryDisplay();
+		const { width, height } = primaryDisplay.workAreaSize;
 
-    const createWindow = () => {
+		const isMobile = height > width;
 
-        const primaryDisplay = screen.getPrimaryDisplay();
-        const { width, height } = primaryDisplay.workAreaSize;
+		mainWindow = new BrowserWindow({
+			width: isMobile ? undefined : 1024,
+			height: isMobile ? undefined : 720,
+			resizable: isMobile ? true : false,
+			autoHideMenuBar: true,
+			frame: true,
+			movable: true,
+			backgroundColor: '#fff',
+			icon: path.join(__dirname, 'build-web', iconFile),
+			webPreferences: {
+				nodeIntegration: true,
+				contextIsolation: false,
+				enableRemoteModule: true,
+				spellcheck: true,
+			},
+		});
 
-        const isMobile = height > width;
+		const zoom = {
+			label: 'Zoom',
+			visible: false,
+			acceleratorWorksWhenHidden: true,
+			submenu: [
+				{
+					label: 'Zoom In',
+					// role: 'zoomIn',
+					accelerator: 'CommandOrControl++',
+				},
+				{
+					label: 'Zoom Out',
+					// role: 'zoomOut',
+					accelerator: 'CommandOrControl+-',
+				},
+				{
+					label: 'Zoom Reset',
+					// role: 'resetZoom',
+					accelerator: 'CommandOrControl+0',
+				},
+			],
+		};
 
-        mainWindow = new BrowserWindow({
-            width: isMobile ? undefined : 1024,
-            height: isMobile ? undefined : 720,
-            resizable: isMobile ? true : false,
-            autoHideMenuBar: true,
-            frame: true,
-            movable: true,
-            backgroundColor: '#fff',
-            icon: path.join(__dirname, 'build-web', iconFile),
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false,
-                enableRemoteModule: true,
-                spellcheck: true
-            }
-        });
+		const edit = {
+			label: 'Edit',
+			visible: true,
+			submenu: [
+				{
+					label: 'Cut',
+					accelerator: 'CmdOrCtrl+X',
+					selector: 'cut:',
+				},
+				{
+					label: 'Copy',
+					accelerator: 'CmdOrCtrl+C',
+					selector: 'copy:',
+				},
+				{
+					label: 'Paste',
+					accelerator: 'CmdOrCtrl+V',
+					selector: 'paste:',
+				},
+				{
+					label: 'Select All',
+					accelerator: 'CmdOrCtrl+A',
+					selector: 'selectAll:',
+				},
+			],
+		};
 
-        const zoom = {
-            label: 'Zoom',
-            visible: false,
-            acceleratorWorksWhenHidden: true,
-            submenu: [
-                {
-                    label: 'Zoom In',
-                    // role: 'zoomIn',
-                    accelerator: 'CommandOrControl++',
-                },
-                {
-                    label: 'Zoom Out',
-                    // role: 'zoomOut',
-                    accelerator: 'CommandOrControl+-',
-                },
-                {
-                    label: 'Zoom Reset',
-                    // role: 'resetZoom',
-                    accelerator: 'CommandOrControl+0',
-                },
-            ]
-        };
+		const selectSpellLanguage = () => {
+			const menuLanguages = new Menu();
 
-        const edit = {
-            label: 'Edit',
-            visible: true,
-            submenu: [
-                {
-                    label: 'Cut',
-                    accelerator: 'CmdOrCtrl+X',
-                    selector: 'cut:'
-                },
-                {
-                    label: 'Copy',
-                    accelerator: 'CmdOrCtrl+C',
-                    selector: 'copy:'
-                },
-                {
-                    label: 'Paste',
-                    accelerator: 'CmdOrCtrl+V',
-                    selector: 'paste:'
-                },
-                {
-                    label: 'Select All',
-                    accelerator: 'CmdOrCtrl+A',
-                    selector: 'selectAll:'
-                }
-            ]
-        };
+			const languages = ['en-US', 'en-GB', 'de', 'es', 'fr', 'it', 'nl', 'pt', 'sv'];
 
-        const selectSpellLanguage = () => {
+			// for (const language of mainWindow.webContents.session.availableSpellCheckerLanguages) {
+			for (const language of languages) {
+				menuLanguages.append(
+					new MenuItem({
+						label: language.toUpperCase(),
+						click: () => mainWindow.webContents.session.setSpellCheckerLanguages([language]),
+					})
+				);
+			}
 
-            const menuLanguages = new Menu();
+			menuLanguages.popup();
+		};
 
-            const languages = ['en-US', 'en-GB', 'de', 'es', 'fr', 'it', 'nl', 'pt', 'sv'];
+		const toggleSpellChecking = () => {
+			if (mainWindow.webContents.session.spellCheckerEnabled) {
+				spellcheckLabel = enableSpellcheck[appLanguage];
+				mainWindow.webContents.session.spellCheckerEnabled = false;
+			} else {
+				spellcheckLabel = disableSpellcheck[appLanguage];
+				mainWindow.webContents.session.spellCheckerEnabled = true;
+			}
+		};
 
-            // for (const language of mainWindow.webContents.session.availableSpellCheckerLanguages) {
-            for (const language of languages) {
-                menuLanguages.append(new MenuItem({
-                    label: language.toUpperCase(),
-                    click: () => mainWindow.webContents.session.setSpellCheckerLanguages([language])
-                }));
-            }
+		mainWindow.webContents.on('context-menu', (event, params) => {
+			if (!params.isEditable) {
+				return;
+			}
 
-            menuLanguages.popup();
-        }
+			// console.log(params)
+			// console.log(mainWindow.webContents.session)
 
-        const toggleSpellChecking = () => {
-            if (mainWindow.webContents.session.spellCheckerEnabled) {
-                spellcheckLabel = enableSpellcheck[appLanguage];
-                mainWindow.webContents.session.spellCheckerEnabled = false;
-            } else {
-                spellcheckLabel = disableSpellcheck[appLanguage];
-                mainWindow.webContents.session.spellCheckerEnabled = true;
-            }
-        }
+			const menu = new Menu();
 
-        mainWindow.webContents.on('context-menu', (event, params) => {
+			for (const suggestion of params.dictionarySuggestions) {
+				menu.append(
+					new MenuItem({
+						label: suggestion,
+						click: () => mainWindow.webContents.replaceMisspelling(suggestion),
+					})
+				);
+			}
 
-            if (!params.isEditable) { return }
+			if (params.dictionarySuggestions.length > 0) {
+				menu.append(
+					new MenuItem({
+						type: 'separator',
+					})
+				);
+			}
 
-            // console.log(params)
-            // console.log(mainWindow.webContents.session)
+			if (process.platform !== 'darwin') {
+				menu.append(
+					new MenuItem({
+						label: selectLanguage[appLanguage],
+						click: () => selectSpellLanguage(),
+					})
+				);
+			}
 
-            const menu = new Menu()
+			menu.append(
+				new MenuItem({
+					label: spellcheckLabel || disableSpellcheck[appLanguage],
+					click: () => toggleSpellChecking(),
+				})
+			);
 
-            for (const suggestion of params.dictionarySuggestions) {
-                menu.append(new MenuItem({
-                    label: suggestion,
-                    click: () => mainWindow.webContents.replaceMisspelling(suggestion)
-                }))
-            }
-
-            if (params.dictionarySuggestions.length > 0) {
-                menu.append(new MenuItem({
-                    type: 'separator'
-                }));
-            }
-
-            if (process.platform !== 'darwin') {
-                menu.append(new MenuItem({
-                    label: selectLanguage[appLanguage],
-                    click: () => selectSpellLanguage()
-                }));
-            }
-
-            menu.append(new MenuItem({
-                label: spellcheckLabel || disableSpellcheck[appLanguage],
-                click: () => toggleSpellChecking()
-            }));
-
-            /*
+			/*
             // Allow users to add the misspelled word to the dictionary
             if (params.misspelledWord) {
                 menu.append(
@@ -209,90 +218,95 @@ if (!hasLock) {
             }
             */
 
-            menu.popup()
-        });
+			menu.popup();
+		});
 
-        protocol.registerFileProtocol('app', (request, callback) => {
+		protocol.registerFileProtocol('app', (request, callback) => {
+			const url = request.url.substr(6);
+			const appPath = path.normalize(`${__dirname}/${url}`);
 
-            const url = request.url.substr(6);
-            const appPath = path.normalize(`${__dirname}/${url}`);
+			callback({ path: appPath });
+		});
 
-            callback({ path: appPath });
-        })
+		mainWindow.loadURL('app://build-web/index.html').catch(_error => null);
 
-        mainWindow.loadURL('app://build-web/index.html').catch(_error => null);
+		// mainWindow.loadFile(path.join(__dirname, 'build-web', 'index.html')).catch(_error => null);
+		// mainWindow.loadFile('build-web/index.html').catch(_error => null);
+		// mainWindow.loadURL('http://localhost:9999').catch(_error => null);
+		// mainWindow.webContents.openDevTools()
 
-        // mainWindow.loadFile(path.join(__dirname, 'build-web', 'index.html')).catch(_error => null);
-        // mainWindow.loadFile('build-web/index.html').catch(_error => null);
-        // mainWindow.loadURL('http://localhost:9999').catch(_error => null);
-        // mainWindow.webContents.openDevTools()
+		mainWindow.on('close', event => {
+			event.preventDefault();
 
-        mainWindow.on('close', event => {
-            event.preventDefault();
+			if (mainWindow) {
+				mainWindow.webContents.send('storeDataAndCloseApp');
+			} else {
+				app.quit();
+			}
+		});
 
-            if (mainWindow) {
-                mainWindow.webContents.send('storeDataAndCloseApp')
-            } else {
-                app.quit();
-            }
-        });
+		const topMenu = {
+			label: app.getName(),
+			submenu: [
+				// { role: 'hide' },
+				// { role: 'hideothers' },
+				// { role: 'unhide' },
+				// { type: 'separator' },
+				{ role: 'quit' },
+			],
+		};
 
-        const topMenu = {
-            label: app.getName(),
-            submenu: [
-                // { role: 'hide' },
-                // { role: 'hideothers' },
-                // { role: 'unhide' },
-                // { type: 'separator' },
-                { role: 'quit' }
-            ]
-        }
+		const menuTemplate = [topMenu, zoom, edit];
 
-        const menuTemplate = [
-            topMenu,
-            zoom,
-            edit
-        ];
+		const menu = Menu.buildFromTemplate(menuTemplate);
 
-        const menu = Menu.buildFromTemplate(menuTemplate);
+		Menu.setApplicationMenu(menu);
+	};
 
-        Menu.setApplicationMenu(menu);
-    }
+	app.whenReady()
+		.then(_response => {
+			createWindow();
 
-    app.whenReady()
-        .then(_response => {
-            createWindow()
+			appLocale = app.getLocale();
+			appLanguage = appLocale.slice(0, 2);
+			if (!['en', 'de', 'fr'].includes(appLanguage)) {
+				appLanguage = 'en';
+			}
 
-            appLocale = app.getLocale();
-            appLanguage = appLocale.slice(0, 2);
-            if (!['en', 'de', 'fr'].includes(appLanguage)) { appLanguage = 'en' }
+			mainWindow.webContents.session.setSpellCheckerLanguages([appLocale]);
 
-            mainWindow.webContents.session.setSpellCheckerLanguages([appLocale]);
+			mainWindow.webContents.session.setPermissionRequestHandler((webContents, permission, callback) => {
+				return callback(true);
+			});
+		})
+		.catch(_error => null);
 
-            mainWindow.webContents.session.setPermissionRequestHandler((webContents, permission, callback) => {
-                return callback(true)
-            });
-        })
-        .catch(_error => null);
+	app.on('second-instance', () => {
+		if (mainWindow) {
+			if (mainWindow.isMinimized()) {
+				mainWindow.restore();
+			}
+			mainWindow.focus();
+		}
+	});
 
-    app.on('second-instance', () => {
-        if (mainWindow) {
-            if (mainWindow.isMinimized()) { mainWindow.restore(); }
-            mainWindow.focus();
-        }
-    });
+	ipcMain.handle('getLocale', () => {
+		return app.getLocale();
+	});
 
-    ipcMain.handle('getLocale', () => { return app.getLocale() });
+	ipcMain.handle('getPath', (_event, dir) => {
+		return app.getPath(dir);
+	});
 
-    ipcMain.handle('getPath', (_event, dir) => { return app.getPath(dir) });
+	ipcMain.on('showSaveDialog', (event, options) => {
+		event.returnValue = dialog.showSaveDialogSync(options);
+	});
 
-    ipcMain.on('showSaveDialog', (event, options) => {
-        event.returnValue = dialog.showSaveDialogSync(options);
-    });
-
-    ipcMain.on('closeApp', () => {
-        if (mainWindow) { mainWindow.removeAllListeners('close') }
-        mainWindow = undefined;
-        app.quit();
-    });
+	ipcMain.on('closeApp', () => {
+		if (mainWindow) {
+			mainWindow.removeAllListeners('close');
+		}
+		mainWindow = undefined;
+		app.quit();
+	});
 }
