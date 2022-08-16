478971053e09c628ee908c7acc232b5af63ae7fc
alariej <alaj@gmx.net>
2022-08-16 20:34:08 +0200

    Changed source of RN cli

--- scripts/react-native.js
+++ scripts/react-native.js
@@ -2,4 +2,4 @@ if (process.argv.includes('run-ios')) {
 	process.argv.push('--device');
 }
 
-require('@react-native-community/cli').run(); // eslint-disable-line
+require('react-native/cli').run(); // eslint-disable-line
