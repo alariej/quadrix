fefd0b8308b62c10ba8839fab4a84ed3d9fc0b47
alariej <alaj@gmx.net>
2022-09-19 20:46:20 +0200

    RN script: Added simulator option

--- scripts/react-native.js
+++ scripts/react-native.js
@@ -1,5 +1,6 @@
 if (process.argv.includes('run-ios')) {
 	process.argv.push('--device');
+	// process.argv.push('--simulator=iPhone 14');
 }
 
 require('react-native/cli').run(); // eslint-disable-line
