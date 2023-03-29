961a9209790ff91d271976ae7ecc210dac0f5e45
alariej <alaj@gmx.net>
2023-03-29 17:49:28 +0200

    RN 0.71.4 (iOS)

--- ios/quadrix/AppDelegate.m
+++ ios/quadrix/AppDelegate.m
@@ -7,11 +7,8 @@
 
 #import "AppDelegate.h"
 
-#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
-
 #import <Firebase.h>
 
 @implementation AppDelegate
@@ -21,23 +18,10 @@
 
   [FIRApp configure];
 
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"RXApp"
-                                            initialProperties:nil];
+  self.moduleName = @"RXApp";
+  self.initialProps = @{};
 
-  if (@available(iOS 13.0, *)) {
-      rootView.backgroundColor = [UIColor systemBackgroundColor];
-  } else {
-      rootView.backgroundColor = [UIColor whiteColor];
-  }
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
