8dec0730898ef38a619efda1d0c366441dc1e682
alariej <alaj@gmx.net>
2023-04-09 20:10:57 +0200

    Config: Raised message count

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -43,7 +43,7 @@ export const PUSH_GATEWAY_URL = 'https://push.quadrix.chat/_matrix/push/v1/notif
 
 // other
 
-export const MESSAGE_COUNT_INC = 100;
-export const MESSAGE_COUNT_ADD = 100;
+export const MESSAGE_COUNT_INC = 120;
+export const MESSAGE_COUNT_ADD = 120;
 export const INACTIVE_DAYS = 30;
 export const CLEAR_DATASTORE_VERSION = '1.4.8';
