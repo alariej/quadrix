3cbd4ebfcf123cd9c0b83f344f00d241f32b1e0c
alariej <alaj@gmx.net>
2023-07-26 08:31:03 +0200

    New spinner

--- src/components/AnimatedSpinner.tsx
+++ src/components/AnimatedSpinner.tsx
@@ -1,161 +1,77 @@
 import * as React from 'react';
 import * as RX from 'reactxp';
+import IconSvg, { SvgFile } from './IconSvg';
 
 interface AnimatedSpinnerProps {
 	color: string;
 	size: number;
 }
 
-const animatedSizeStart = 1;
-const animatedSizeStop = 0.667;
-const animatedDuration = 250;
-const loopDuration = 1500;
-const easing = RX.Animated.Easing.Out();
-
 export default class AnimatedSpinner extends RX.Component<AnimatedSpinnerProps, RX.Stateless> {
-	private animatedScale1: RX.Animated.Value;
-	private animatedScale2: RX.Animated.Value;
-	private animatedScale3: RX.Animated.Value;
-	private animatedStyle1: RX.Types.AnimatedViewStyleRuleSet;
-	private animatedStyle2: RX.Types.AnimatedViewStyleRuleSet;
-	private animatedStyle3: RX.Types.AnimatedViewStyleRuleSet;
-	private animation1: RX.Types.Animated.CompositeAnimation;
-	private animation2: RX.Types.Animated.CompositeAnimation;
-	private animation3: RX.Types.Animated.CompositeAnimation;
-	private animation4: RX.Types.Animated.CompositeAnimation;
-	private _isMounted = false;
+	private animatedValue: RX.Animated.Value;
+	private animatedStyle: RX.Types.AnimatedViewStyleRuleSet;
+	private animatedLoop: RX.Types.Animated.LoopConfig;
 
 	constructor(props: AnimatedSpinnerProps) {
 		super(props);
 
-		this.animatedScale1 = RX.Animated.createValue(animatedSizeStart);
-		this.animatedScale2 = RX.Animated.createValue(animatedSizeStart);
-		this.animatedScale3 = RX.Animated.createValue(animatedSizeStart);
-
-		this.animatedStyle1 = RX.Styles.createAnimatedViewStyle({
-			transform: [{ scale: this.animatedScale1 }],
+		this.animatedValue = RX.Animated.createValue(0);
+		this.animatedStyle = RX.Styles.createAnimatedViewStyle({
+			transform: [
+				{
+					rotate: this.animatedValue.interpolate({
+						inputRange: [0, 1],
+						outputRange: ['0deg', '360deg'],
+					}),
+				},
+			],
 		});
-		this.animatedStyle2 = RX.Styles.createAnimatedViewStyle({
-			transform: [{ scale: this.animatedScale2 }],
-		});
-		this.animatedStyle3 = RX.Styles.createAnimatedViewStyle({
-			transform: [{ scale: this.animatedScale3 }],
-		});
-
-		this.animation1 = RX.Animated.timing(this.animatedScale1, {
-			duration: animatedDuration,
-			toValue: animatedSizeStop,
-			easing: easing,
-			useNativeDriver: true,
-		});
-
-		this.animation2 = RX.Animated.timing(this.animatedScale2, {
-			duration: animatedDuration,
-			toValue: animatedSizeStop,
-			easing: easing,
-			useNativeDriver: true,
-		});
-
-		this.animation3 = RX.Animated.timing(this.animatedScale3, {
-			duration: animatedDuration,
-			toValue: animatedSizeStop,
-			easing: easing,
-			useNativeDriver: true,
-		});
-
-		this.animation4 = RX.Animated.parallel([
-			RX.Animated.timing(this.animatedScale1, {
-				duration: animatedDuration,
-				toValue: animatedSizeStart,
-				easing: easing,
-				useNativeDriver: true,
-			}),
-			RX.Animated.timing(this.animatedScale2, {
-				duration: animatedDuration,
-				toValue: animatedSizeStart,
-				easing: easing,
-				useNativeDriver: true,
-			}),
-			RX.Animated.timing(this.animatedScale3, {
-				duration: animatedDuration,
-				toValue: animatedSizeStart,
-				easing: easing,
-				useNativeDriver: true,
-			}),
-		]);
+		this.animatedLoop = {
+			restartFrom: 0,
+		};
 	}
-
-	private animateSpinner = () => {
-		this.animation1?.start(() => {
-			this.animation2?.start(() => {
-				this.animation3?.start(() => {
-					this.animation4?.start();
-				});
-			});
-		});
-
-		if (this._isMounted) {
-			setTimeout(() => {
-				this.animateSpinner();
-			}, loopDuration);
-		}
-	};
 
 	public componentDidMount(): void {
-		this._isMounted = true;
-		this.animateSpinner();
-	}
-
-	public componentWillUnmount(): void {
-		this._isMounted = false;
+		RX.Animated.timing(this.animatedValue, {
+			duration: 1000,
+			toValue: 1,
+			easing: RX.Animated.Easing.Linear(),
+			loop: this.animatedLoop,
+			useNativeDriver: true,
+		}).start();
 	}
 
 	public render(): JSX.Element | null {
+		const borderRadius = 1;
+		const arcWidth = this.props.size / 2 - borderRadius;
+
 		return (
 			<RX.View
 				style={{
-					flex: 1,
-					flexDirection: 'row',
-					alignItems: 'center',
-					justifyContent: 'center',
+					height: this.props.size,
+					width: this.props.size,
+					borderRadius: this.props.size / 2,
 				}}
 			>
 				<RX.Animated.View
 					style={[
-						this.animatedStyle1,
+						this.animatedStyle,
 						{
-							height: this.props.size / 3,
-							width: this.props.size / 3,
-							borderRadius: this.props.size / 15,
-							margin: 0,
-							backgroundColor: this.props.color,
+							height: this.props.size,
+							width: this.props.size,
+							borderRadius: this.props.size / 2,
+							borderWidth: borderRadius,
+							borderColor: this.props.color,
 						},
 					]}
-				/>
-				<RX.Animated.View
-					style={[
-						this.animatedStyle2,
-						{
-							height: this.props.size / 3,
-							width: this.props.size / 3,
-							borderRadius: this.props.size / 15,
-							margin: 0,
-							backgroundColor: this.props.color,
-						},
-					]}
-				/>
-				<RX.Animated.View
-					style={[
-						this.animatedStyle3,
-						{
-							height: this.props.size / 3,
-							width: this.props.size / 3,
-							borderRadius: this.props.size / 15,
-							margin: 0,
-							backgroundColor: this.props.color,
-						},
-					]}
-				/>
+				>
+					<IconSvg
+						source={require('../resources/svg/arc.json') as SvgFile}
+						height={arcWidth}
+						width={arcWidth}
+						fillColor={this.props.color}
+					/>
+				</RX.Animated.View>
 			</RX.View>
 		);
 	}
