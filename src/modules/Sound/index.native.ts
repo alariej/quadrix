f69f304cf4fdd8370be746286935d2cedd2a7042
alariej <alaj@gmx.net>
2023-09-15 15:18:52 +0200

    Re-changed remote element call url

--- src/modules/Sound/index.native.ts
+++ src/modules/Sound/index.native.ts
@@ -4,6 +4,11 @@ class Sound {
 	public play(name: string) {
 		RNSound.setCategory('Playback');
 		const sound = new RNSound(name + '.wav', RNSound.MAIN_BUNDLE, () => {
+			
+
+			// Mixwithothers needed to prevent music from stopping
+
+			
 			sound.play(() => {
 				sound.release();
 			});
